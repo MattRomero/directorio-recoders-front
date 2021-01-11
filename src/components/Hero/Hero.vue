e0bf0ff573200ebf769a987cfb45a1571e6e8c06
MattRomero <mromero.olivos@gmail.com>
2021-01-11 18:53:17 -0300

    Styling hero text

--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -1,7 +1,8 @@
 <template>
     <section class="hero-home">
         <div class="hero-home__display-text">
-            <h1>Sé parte de la<br>comunidad de recoders</h1>
+            <h1 class="hero-home__display-title">Sé parte de la<br>comunidad de recoders</h1>
+            <h3 class="hero-home__display-subtitle">Completa el formulario y únete a la comunidad más<br>grande de reconvertidos digitales</h3>
         </div>
         <div class="hero-home__image">
             <img src="../../assets/hero-imagen.svg" class="main-navbar__logo" alt="Recoders logo" >
