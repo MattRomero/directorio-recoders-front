7429e03929ac13310dd67d87a0e134fedbdb6309
MattRomero <mromero.olivos@gmail.com>
2021-01-11 18:56:51 -0300

    .

--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="hero-home">
         <div class="hero-home__display-text">
-            <h1 class="hero-home__display-title">Sé parte de la<br>comunidad de recoders</h1>
+            <h1 class="hero-home__display-title">Sé parte de la<br>comunidad de recoders.</h1>
             <h3 class="hero-home__display-subtitle">Completa el formulario y únete a la comunidad más<br>grande de reconvertidos digitales</h3>
         </div>
         <div class="hero-home__image">
