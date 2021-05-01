dc928933ca316cc47d7c83828aa973fc1a5c28a4
Matías Romero <mromero.olivos@gmail.com>
2021-05-01 01:17:09 -0400

    Added font-awesome support

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import store from './store'
 import makeServer from "./mirage_api/server";
 import vuetify from './plugins/vuetify';
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 makeServer();
 
--- src/mirage_api/server.js
+++ src/mirage_api/server.js
@@ -52,48 +52,96 @@ export default function () {
                         name: "Matías Romero",
                         description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu", 
                         skillset: [1],
+                        website: "https://website.com",
+                        location: "Santiago",
+                        icons: [
+                            { icon: "fab fa-github", link: "https://github.com", name: "Github"},
+                            { icon: "fab fa-behance", link: "https://behance.com", name: "Behance" }
+                        ]
                     },
                     { 
                         id: 2,
                         name: "Carlos Guzman", 
                         description: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large.",
                         skillset: [1],
+                        website: "https://website.com",
+                        location: "Santiago",
+                        icons: [
+                            { icon: "fab fa-github", link: "https://github.com", name: "Github"},
+                            { icon: "fab fa-behance", link: "https://behance.com", name: "Behance" }
+                        ]
                     },
                     { 
                         id: 3, 
                         name: "Matías Criado", 
                         description: "The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox nymph",
-                        skillset: [1]
+                        skillset: [1],
+                        website: "https://website.com",
+                        location: "Santiago",
+                        icons: [
+                            { icon: "fab fa-github", link: "https://github.com", name: "Github"},
+                            { icon: "fab fa-behance", link: "https://behance.com", name: "Behance" }
+                        ]
                     },
                     { 
                         id: 4,
                         name: "Natalia Bustamante",
                         description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu",
-                        skillset: [3,4]
+                        skillset: [3,4],
+                        website: "https://website.com",
+                        location: "Santiago",
+                        icons: [
+                            { icon: "fab fa-github", link: "https://github.com", name: "Github"},
+                            { icon: "fab fa-behance", link: "https://behance.com", name: "Behance" }
+                        ]
                     },
                     { 
                         id: 5,
-                        name: "Fernanda", 
+                        name: "Fernanda Silva", 
                         description: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large.", 
-                        skillset: [3,4]
+                        skillset: [3,4],
+                        website: "https://website.com",
+                        location: "Santiago",
+                        icons: [
+                            { icon: "fab fa-github", link: "https://github.com", name: "Github"},
+                            { icon: "fab fa-behance", link: "https://behance.com", name: "Behance" }
+                        ]
                     },
                     { 
                         id: 6, 
                         name: "Eduardo Prieto", 
                         description: "The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox nymph",
-                        skillset: [3,4]
+                        skillset: [3,4],
+                        website: "https://website.com",
+                        location: "Santiago",
+                        icons: [
+                            { icon: "fab fa-github", link: "https://github.com", name: "Github"},
+                            { icon: "fab fa-behance", link: "https://behance.com", name: "Behance" }
+                        ]
                     },
                     { 
                         id: 7,
                         name: "Paz Parry",
                         description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu",
-                        skillset: [1,2,3,4]
+                        skillset: [1,2,3,4],
+                        website: "https://website.com",
+                        location: "Santiago",
+                        icons: [
+                            { icon: "fab fa-github", link: "https://github.com", name: "Github"},
+                            { icon: "fab fa-behance", link: "https://behance.com", name: "Behance" }
+                        ]
                     },
                     { 
                         id: 8,
                         name: "Felipe Riquelme", 
                         description: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large.", 
-                        skillset: [1,2]
+                        skillset: [1,2],
+                        website: "https://website.com",
+                        location: "Santiago",
+                        icons: [
+                            { icon: "fab fa-github", link: "https://github.com", name: "Github"},
+                            { icon: "fab fa-behance", link: "https://behance.com", name: "Behance" }
+                        ]
                     },
                 ],
             }))
